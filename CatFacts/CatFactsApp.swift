703c758bf6e40d9161f04dba706a9e1225479486
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-26 11:52:34 +0000

    Initial Commit

---
+++ CatFacts/CatFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsApp.swift
+//  CatFacts
+//
+//  Created by Don Bouncy on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
